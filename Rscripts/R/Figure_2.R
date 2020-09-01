b794d14cc428ce28322b339bac5e3533c34ef38a
Joselyn Chavez <joselynchavezf@gmail.com>
2020-08-31 20:16:55 -0500

    remove archaeas and adjust image size

--- Rscripts/R/Figure_2.R
+++ Rscripts/R/Figure_2.R
@@ -12,6 +12,18 @@ total_cogs <- readxl::read_excel(here::here("data/Table_S3.xlsx"),
                                  skip = 2)
 
 colnames(total_cogs)[6:7] <- c("tf", "sf")
+colnames(total_cogs)[5] <- "ORFs"
+
+## remove archaeas
+
+archaeas <- c("Crenarchaeota", "Euryarchaeota")
+total_cogs <- dplyr::filter(total_cogs, !phylum %in% archaeas)
+
+## reorder by phylum
+phylum_order <- sort(unique(total_cogs$phylum))[-c(3,8)]
+phylum_order[8:9] <- c("Chlamydiae" , "Tenericutes" )
+
+total_cogs$phylum <- factor(total_cogs$phylum, ordered = TRUE, levels = phylum_order)
 
 ## plot the number of transcription factors versus sigma factors per genome
 
@@ -19,8 +31,6 @@ TheilSen <- function(..., weights = NULL) {
     mblm::mblm(...)
 }
 
-tiff(filename = here::here("figures","F2.tiff"),
-     width = 18, height = 12, units = "cm", res = 300)
 ggplot(total_cogs, aes(x = tf, y = sf)) +
     geom_point(size = 0.2) +
     geom_smooth(method = "TheilSen", se = FALSE, size = 0.5) +
@@ -32,19 +42,19 @@ ggplot(total_cogs, aes(x = tf, y = sf)) +
           axis.text = element_text(size = 8),
           strip.text = element_text(size = 8, face = "bold")) +
     facet_wrap(~phylum, scales = "free")
-dev.off()
+ggsave(filename = here::here("figures","F2.tiff"), device = "tiff",
+       width = 9, height = 6, units = "cm", dpi = 300, scale = 2)
 
 ### obtain lm coefficients per phylum
-total_cogs %>%
-    group_by(phylum) %>%
-    summarise(cor = cor(`Sigma factors`, `Transcription factors`))
 
-x <- total_cogs %>% filter(phylum == "Verrucomicrobia")
+slopeThielsen <- function(x) {
+    df <- total_cogs %>% dplyr::filter(phylum == x)
+    m <- mblm::mblm(sf ~ tf ,df)[[1]][2] %>% round(2)
+    names(m) <- x
+    return(m)
+}
 
-total_cogs %>%
-    group_by(phylum) %>%
-    summarise(m = mblm::mblm(sf ~ tf ) )
-
+sapply(levels(total_cogs$phylum), slopeThielsen)
 
 model <- mblm::mblm(formula = x$`Sigma factors` ~ x$`Transcription factors`)
 mblm::mblm(formula = total_cogs$`Sigma factors` ~ total_cogs$`Transcription factors`, dataframe = total_cogs)
